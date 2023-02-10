bdc17a60787c5f0c5b25eeba542a387758f28af6
barsu5489 <ebarsulai@gmail.com>
2023-02-11 01:30:14 +0300

    checks if sentence ends with a fullstop using self keyword and end_with method

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,9 +1,12 @@
 require 'pry'
 
 class String
-
+   
+  def report_on_self
+    "Self is: #{self}"
+  end
   def sentence?
-    
+    "#{self}".end_with?('.') 
   end
 
   def question?
@@ -18,3 +21,4 @@ class String
 
   end
 end
+puts "hello.".sentence?
