9e03ca9cd25f3e7a5dc2e0534622e201a1a04e79
barsu5489 <ebarsulai@gmail.com>
2023-02-11 23:55:31 +0300

    rubocop

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
+
 require 'pry'
 
 class String
-   
   def report_on_self
     "Self is: #{self}"
   end
+
   def sentence?
-    "#{self}".end_with?('.') 
+    to_s.end_with?('.')
   end
 
   def question?
-  "#{self}".end_with?('?'|| '?')
+    to_s.end_with?('?' || '?')
   end
 
   def exclamation?
-    "#{self}".end_with?('!')
+    to_s.end_with?('!')
   end
 
   def count_sentences
-    "#{self}".to_s.split(' ').length
+    to_s.to_s.split(' ').length
   end
 end
-puts "hello.".sentence?
 print 'This, well, is a sentence. This is too!! And so is this, I think? Woo...'.count_sentences
